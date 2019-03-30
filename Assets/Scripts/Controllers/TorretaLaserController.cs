091318d700cee1e03aa9dc45f4010b460a98bf81
Jesús Salvatierra Domínguez <jesalvat@ucm.es>
2019-03-30 17:09:02 +0100

    Creada la escena TorretaLaser y los scripts TorretaLaserController y TorretaLaserShooting

---
+++ Assets/Scripts/Controllers/TorretaLaserController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorretaLaserController : TorretaController {
+
+    private LineRenderer lr;
+    private GameObject shootingPoint;
+    Vector2 laserHitPos = Vector2.zero;
+
+    private void Awake()
+    {
+        lr = GetComponentInChildren<LineRenderer>();
+        shootingPoint = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (state == EnemyState.Shooting)
+        {
+            lr.enabled = true;
+            lr.SetPosition(0, shootingPoint.transform.position);
+            lr.SetPosition(1, player.transform.position);
+        }
+        else
+            lr.enabled = false;
+	}
+
+    public override void Sight(RaycastHit2D sight)
+    {
+        base.Sight(sight);
+    }
+}
---
+++ Assets/Scripts/Shooting/TorretaLaserShooting.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorretaLaserShooting : Shooting {
+
+
+    // Update is called once per frame
+    public override void  Update () {
+        Vector2 lookDirection = (player.position) - transform.position;
+        float angle = Mathf.Atan(lookDirection.y / lookDirection.x) * (180 / Mathf.PI);
+
+        transform.eulerAngles = new Vector3(0, 0, angle + (lookDirection.x < 0f ? 180f : 0f));
+    }
+}
