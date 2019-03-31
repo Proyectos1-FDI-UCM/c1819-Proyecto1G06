6ee72d1e115a8ef394d5dce4aa96f6910a141fa8
Daniel Cortijo Gamboa <dcortijo@ucm.es>
2019-03-31 18:43:16 +0200

    Añadida interfaz de los ataques del boss.

---
+++ Assets/Scripts/Boss/SweepingLaser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SweepingLaser : MonoBehaviour, IBossAttack {
+
+    LineRenderer lr;
+
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    public void Attack()
+    {
+        
+    }
+
+}
