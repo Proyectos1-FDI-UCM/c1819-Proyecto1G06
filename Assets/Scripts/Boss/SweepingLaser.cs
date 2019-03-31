bee9b3731e976c637dfe74b8c7f2f6a052885376
Daniel Cortijo Gamboa <dcortijo@ucm.es>
2019-03-31 19:38:17 +0200

    Añadido el láser de barrido.

--- Assets/Scripts/Boss/SweepingLaser.cs
+++ Assets/Scripts/Boss/SweepingLaser.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SweepingLaser : MonoBehaviour, IBossAttack {
+public class SweepingLaser : MonoBehaviour {
 
     LineRenderer lr;
+    public Transform shootingPoint;
+    public Transform followPoint;
+    RaycastHit2D hit;
 
     void Awake()
     {
         lr = GetComponent<LineRenderer>();
     }
 
-    public void Attack()
+    void OnEnable()
     {
-        
+        lr.enabled = true;
+        lr.SetPosition(0, shootingPoint.position);
+        lr.SetPosition(1, followPoint.position);
+    }
+
+    void Update()
+    {
+        lr.SetPosition(1, followPoint.position);
+        hit = Physics2D.Linecast(shootingPoint.position, followPoint.position, LayerMask.GetMask("Player"));
+        if(hit.transform.GetComponent<PlayerHealth>() != null)
+        {
+            hit.transform.GetComponent<PlayerHealth>().TakeDamage();
+        }
+    }
+
+    private void OnDisable()
+    {
+        followPoint.position = shootingPoint.position;
+        lr.enabled = false;
     }
 
 }
