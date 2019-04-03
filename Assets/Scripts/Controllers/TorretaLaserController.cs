ca2d4c443b856881e135c23914646fc750febcc9
David Brey Plaza <dbrey@ucm.es>
2019-04-03 10:50:34 +0200

    Cambios desde laboratorio (Sin exito)

--- Assets/Scripts/Controllers/TorretaLaserController.cs
+++ Assets/Scripts/Controllers/TorretaLaserController.cs
@@ -6,13 +6,15 @@ public class TorretaLaserController : TorretaController {
 
     public float lerpTime;
 
-    private Vector3 startPosition, currentPosition;
+    private Vector3 startPosition;
 
     LineRenderer lr;
 
     GameObject head;
 
     public Material redLaser, orangeLaser;
+
+    public Transform followPoint;
 
     bool shot = false;
 
@@ -32,9 +34,10 @@ public class TorretaLaserController : TorretaController {
 
     public void Update()
     {
-        currentPosition = Vector3.Lerp(startPosition, player.transform.position, (Time.time) / lerpTime);
         lr.SetPosition(0, head.transform.position);
-        lr.SetPosition(1, currentPosition);
+        lr.SetPosition(1, followPoint.position);
+
+        Debug.DrawLine(head.transform.position, followPoint.position, Color.yellow);
 
         if (state == EnemyState.Shooting)
         {
@@ -42,11 +45,13 @@ public class TorretaLaserController : TorretaController {
 
             if (!shot)
             {
-                Vector2 lookDirection = (currentPosition) - transform.position;
+                Vector2 lookDirection = followPoint.position - transform.position;
                 float angle = Mathf.Atan(lookDirection.y / lookDirection.x) * (180 / Mathf.PI);
 
                 head.transform.eulerAngles = new Vector3(0, 0, angle + (lookDirection.x < 0f ? 180f : 0f));
-                CheckCollisions();
+
+                
+                   CheckCollisions();
             }
         }
         else
@@ -56,34 +61,26 @@ public class TorretaLaserController : TorretaController {
         }
     }
 
-    public Vector3 Lerp(Vector3 start, Vector3 end, float timeStartedLerping, float lerpTime)
-    {
-        float timeSinceStarted = Time.time - timeStartedLerping;
-        float percentageComplete = timeSinceStarted / lerpTime;
-
-        Vector3 result = Vector3.Slerp(start, end, percentageComplete);
-
-        return result;
-    }
-
     void CheckCollisions()
     {
-        hit = Physics2D.Linecast(head.transform.position, currentPosition, LayerMask.GetMask("Player"));
-        if (hit.transform.GetComponent<PlayerHealth>() != null)
+        hit = Physics2D.Linecast(head.transform.position, followPoint.position);
+        
+        if (hit.transform.GetComponent<PlayerHealth>() != null) //No detecta la colision (Opciones, linecast, raycast)
         {
-            hit.transform.GetComponent<PlayerHealth>().TakeDamage();
-            Debug.Log(hit.transform.GetComponent<PlayerHealth>());
+            player.GetComponent<PlayerHealth>().TakeDamage();
             ShootLaser();
             Invoke("StopShootingLaser", 0.5f);
         }
+
     }
 
     void ShootLaser()
     {
         shot = true;
         lr.material = redLaser;
-        lr.startWidth = 0.2f;
-        lr.endWidth = 0.2f;
+        lr.startWidth = 0.5f;
+        lr.endWidth = 0.5f;
+        print("Funciona");
     }
 
     void StopShootingLaser()
