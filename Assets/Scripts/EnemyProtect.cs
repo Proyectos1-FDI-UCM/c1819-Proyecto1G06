221b30c105c8213a0092da223962d5bdd6d787fd
David Brey <David Brey Plaza>
2019-04-04 19:27:11 +0200

    Actualizado la rama DronBlindado (Ahora hace dano de contacto y se interpone entre jugador y enemigo)

---
+++ Assets/Scripts/EnemyProtect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProtect : MonoBehaviour
+{
+    public Transform Amigo;
+    private Transform player;
+
+    RaycastHit2D vectorEJ;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameManager.instance.player.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vectorEJ = Physics2D.Linecast(player.position, Amigo.position);
+        Debug.DrawLine(player.position, Amigo.position);
+
+
+        transform.position = vectorEJ.transform.position / 2;
+
+        /*vectorEJ = Physics2D.Linecast(player.position, Amigo.position);
+        Debug.DrawLine(player.position, Amigo.position);
+        if (transform.position != vectorEJ.transform.position / 2)
+        {
+            transform.position =
+        }
+        else
+        {
+            transform.position = vectorEJ.transform.position / 2;
+        }*/
+
+    }
+}
--- README.md
+++ README.md
@@ -4,10 +4,8 @@ Este es el Proyecto 1 del grupo 06 18/19.
 
 ## Sobre esta rama
 
-Esta es la rama principal, donde está la última versiún estable del proyecto.
+Esta es la rama donde trabajamos para crear un enemigo lento que proteja a otro enemigo
 
 ## Cambios
 
-- Actualizado el sprite del jugador.
-- Añadido al Voluntario.
-- Añadido el nivel 2.
+- Actualizado la rama DronBlindado (Hace daño de contacto y se interpone entre jugador y enemigo)
