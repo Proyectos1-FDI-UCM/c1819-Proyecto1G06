a3e086d48a6242a053a8a187e1ae43f21cfdc514
GonzaloFdezMoreno <gonzaf05@ucm.es>
2019-04-07 19:20:18 +0200

    El nivel uno carga al presionar el botón Start Game

--- Assets/Scripts/Menu/LevelManager.cs
+++ Assets/Scripts/Menu/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
@@ -13,4 +14,8 @@ public class LevelManager : MonoBehaviour {
 	void Update () {
 		
 	}
+    public void CargaNivel(string nombreNivel)
+    {
+        SceneManager.LoadScene(nombreNivel);
+    }
 }
