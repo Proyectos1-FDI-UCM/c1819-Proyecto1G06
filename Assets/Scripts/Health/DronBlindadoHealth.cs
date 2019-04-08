50abf862bb099f85bff559f773e10a775b703c76
Eloy Moreno Cortijo <eloymore@ucm.es>
2019-04-08 18:36:29 +0200

    Arreglada la muerte del Dron Blindado.

--- Assets/Scripts/Health/DronBlindadoHealth.cs
+++ Assets/Scripts/Health/DronBlindadoHealth.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class DronBlindadoHealth : EnemyHealth
 {
-    GameObject parent;
     public override void Die()
     {
-        SendMessageUpwards("EnemyDied", transform, SendMessageOptions.DontRequireReceiver);
-        parent = GetComponentInParent<Transform>().gameObject;
-        Destroy(parent);
+        Destroy(transform.parent.gameObject);
+        SendMessageUpwards("EnemyDied", transform.parent, SendMessageOptions.DontRequireReceiver);
     }
 }
