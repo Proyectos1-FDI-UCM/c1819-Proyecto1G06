d1081506fd340049cdb74dccbe53b9aa5b44f224
Daniel Cortijo Gamboa <dcortijo@ucm.es>
2019-04-12 19:27:11 +0200

    Cambiada lo funcionalidad de la bobina.

---
+++ Assets/Scripts/Items/CompanionDamageArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionDamageArea : MonoBehaviour {
+
+    public float timeBetweenDamage;
+    public float Damage;
+
+    float timer;
+
+    private void Start()
+    {
+        GetComponent<FollowTarget>().target = GameManager.instance.player.transform;
+        timer = timeBetweenDamage;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        EnemyHealth health = other.GetComponent<EnemyHealth>();
+        if(health != null && timer == 0f)
+        {
+            health.TakeDamage(Damage);
+            timer = timeBetweenDamage;
+        }
+    }
+
+    private void Update()
+    {
+        if (timer > 0f) timer -= Time.deltaTime;
+        else timer = 0f;
+    }
+
+}
