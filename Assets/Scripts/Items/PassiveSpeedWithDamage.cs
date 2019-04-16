554aaaccdff6918ad275f33bea7263df4b46190f
Daniel Cortijo Gamboa <dcortijo@ucm.es>
2019-04-16 20:47:40 +0200

    El Antifreeze ahora añade parte de tu daño a tu velocidad.

---
+++ Assets/Scripts/Items/PassiveSpeedWithDamage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveSpeedWithDamage : MonoBehaviour, IItem {
+
+    [Range(0f, 1f)] public float percentageConversion;
+
+    float currSpeed = 0;
+
+	public void PickEffect()
+    {
+        GameManager.instance.onPlayerAddedDamage += AddDamageToSpeed;
+        AddDamageToSpeed(0f);
+    }
+
+    public void AddDamageToSpeed(float damage)
+    {
+        GameManager.instance.player.GetComponent<PlayerMovement>().AddSpeed(- currSpeed);
+        currSpeed = GameManager.instance.player.GetComponentInChildren<PlayerShooting>().baseDamage * percentageConversion;
+        GameManager.instance.player.GetComponent<PlayerMovement>().AddSpeed(currSpeed);
+    }
+}
