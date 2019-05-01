53e360a1877029eb6864b809691108405c8eedf9
Eloy Moreno Cortijo <eloymore@ucm.es>
2019-05-01 22:14:13 +0200

    Creada terminal de controles en el primer nivel.

---
+++ Assets/Scripts/Controllers/ControlsScreenController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsScreenController : MonoBehaviour {
+
+    Animator anim;
+    public TextMesh UpText, DownText, LeftText, RightText, ShootText;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        anim.SetBool("ShowControls", true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        anim.SetBool("ShowControls", false);
+    }
+}
