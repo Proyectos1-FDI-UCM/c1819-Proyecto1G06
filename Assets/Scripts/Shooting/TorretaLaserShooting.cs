8b22bc1987b64564ac80ee9389a2bf1f40f613ef
Daniel Cortijo Gamboa <dcortijo@ucm.es>
2019-05-02 20:47:40 +0200

    Arreglado láser de la torreta láser.

--- Assets/Scripts/Shooting/TorretaLaserShooting.cs
+++ Assets/Scripts/Shooting/TorretaLaserShooting.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class TorretaLaserShooting : MkIShooting {
 
     bool damaging = false;
+    LineRenderer lr;
+
+    public override void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        base.Awake();
+    }
 
     public override void Start()
     {
@@ -13,30 +20,21 @@ public class TorretaLaserShooting : MkIShooting {
 
     public override void Update()
     {
-        if (!shooting && controller.GetPlayerDetected())
+        if (!shooting)
         {
-            Vector2 lookDirection = player.position - transform.position;
-            float angle = Mathf.Atan(lookDirection.y / lookDirection.x) * (180 / Mathf.PI) + (lookDirection.x < 0f ? 180f : 0f);
-
-            Vector2 bodyLookDir = player.position - body.position;
-            float bodyAngle = Mathf.Atan(bodyLookDir.y / bodyLookDir.x) * (180 / Mathf.PI) + (bodyLookDir.x < 0f ? 180f : 0f);
-
-            if (bodyAngle > 90 || bodyAngle < -90)
+            if (controller.GetPlayerDetected())
             {
-                // Hacer que no tenga un movimiento poco natural
-                //transform.localScale = new Vector3(-1, 1, 1);
-                body.localScale = new Vector3(1, 1, 1);
-                sprite.flipY = true;
+                MoveTurret();
+
+                lr.SetPosition(0, shootingPoint.transform.position);
+                lr.enabled = true;
             }
             else
             {
-                //transform.localScale = new Vector3(1, 1, 1);
-                body.localScale = new Vector3(-1, 1, 1);
-                sprite.flipY = false;
+                lr.enabled = false;
             }
-
-            transform.eulerAngles = new Vector3(0, 0, angle);
         }
+
         if (damaging)
         {
            
@@ -46,6 +44,8 @@ public class TorretaLaserShooting : MkIShooting {
                 GameManager.instance.onPlayerTookDamage();
             }
         }
+
+        lr.SetPosition(1, Physics2D.Raycast(shootingPoint.transform.position, transform.right, Mathf.Infinity, LayerMask.GetMask("Player", "Environment")).point);
     }
 
     public void StartDamaging()
@@ -62,5 +62,30 @@ public class TorretaLaserShooting : MkIShooting {
     {
         shooting = false;
         ResetCooldown();
+    }
+
+    void MoveTurret()
+    {
+        Vector2 lookDirection = player.position - transform.position;
+        float angle = Mathf.Atan(lookDirection.y / lookDirection.x) * (180 / Mathf.PI) + (lookDirection.x < 0f ? 180f : 0f);
+
+        Vector2 bodyLookDir = player.position - body.position;
+        float bodyAngle = Mathf.Atan(bodyLookDir.y / bodyLookDir.x) * (180 / Mathf.PI) + (bodyLookDir.x < 0f ? 180f : 0f);
+
+        if (bodyAngle > 90 || bodyAngle < -90)
+        {
+            // Hacer que no tenga un movimiento poco natural
+            //transform.localScale = new Vector3(-1, 1, 1);
+            body.localScale = new Vector3(1, 1, 1);
+            sprite.flipY = true;
+        }
+        else
+        {
+            //transform.localScale = new Vector3(1, 1, 1);
+            body.localScale = new Vector3(-1, 1, 1);
+            sprite.flipY = false;
+        }
+
+        transform.eulerAngles = new Vector3(0, 0, angle);
     }
 }
