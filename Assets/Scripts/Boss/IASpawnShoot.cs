ff344da778e95d95d8b4152e3038855f77f2b2de
Daniel Cortijo Gamboa <dcortijo@ucm.es>
2019-05-15 18:52:38 +0200

    Cambiado el funcionamiento del span de la IA, con un enemigo diseñado para ello.

---
+++ Assets/Scripts/Boss/IASpawnShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IASpawnShoot : Shooting {
+
+    public Transform[] cannons;
+
+    public override void Update()
+    {
+        base.Update();
+        if (shootCooldown == 0f && !disarmed) Shoot(); 
+    }
+
+    public override void Shoot()
+    {
+        for(int i = 0; i < cannons.Length; i++)
+        {
+            ResetCooldown();
+            Shoot(i);
+        }
+    }
+
+    public void Shoot(int i)
+    {
+        BulletMovement newBullet = Instantiate<BulletMovement>(bulletPrefab, cannons[i].position, Quaternion.identity, bulletPool);
+        newBullet.Rotate(cannons[i].right);
+    }
+}
