bb2c3521b2598aac3a06e65a4a55ad4286c897c5
David Brey <David Brey Plaza>
2019-05-16 18:12:24 +0200

    Banda sonora anadida

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip intro;
+    public AudioClip battle;
+    public AudioClip vsIA;
+    AudioSource musica;
+    public static MusicManager instance;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            musica = GetComponent<AudioSource>();
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        else Destroy(gameObject);
+
+    }
+
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        switch (scene.name)
+        {
+        case "Nivel 1":
+        case "Nivel 2":
+                if (musica.clip != battle) //Si esta ejecutando el audio battle, se evita que se reinicie el audio
+                {
+                    musica.clip = battle;
+                    musica.Play();
+                }
+        break;
+        
+        case "Nivel IA":
+                musica.clip = vsIA;
+                musica.Play();
+        break;
+
+        case "Menu":
+        case "ModoDios":
+                if (musica.clip != intro)
+                {
+                    musica.clip = intro;
+                    musica.Play();
+                }
+        break;
+
+            
+        }
+    }
+}
